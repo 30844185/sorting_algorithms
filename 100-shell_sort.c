9ddc190bfcf98cd41fd560decf7cc5cf087ec6f7
30844185 <danielhika05@gmail.com>
2023-04-25 08:32:11 -0700

    quick sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,20 +7,20 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	int gap = 1, y, z, tmp;
+	int gap = 1, i, j, tmp;
 
 	while (gap < ((int)(size)))
 		gap = (3 * gap) + 1;
 	for (gap = (gap - 1) / 3; gap > 0; gap = (gap - 1) / 3)
 	{
-		for (y = gap; y < (y nt)size; y += 1)
+		for (i = gap; i < (int)size; i += 1)
 		{
-			tmp = array[y];
+			tmp = array[i];
 
-			for (z = y; z >= gap && array[z - gap] > tmp; z -= gap)
-				array[z] = array[z - gap];
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+				array[j] = array[j - gap];
 
-			array[z] = tmp;
+			array[j] = tmp;
 		}
 		print_array(array, size);
 	}
